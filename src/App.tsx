a6a6a87d00a37c0b7953c464af8abc04015e92d9
Sachin Kumar Verma <sachin.verma@programmers.io>
2022-07-28 06:45:04 +0530

    Updated commented codes finally

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          {/* <Route path='/store' element={<Store />} /> */}
+          <Route path='/store' element={<Store />} />
         </Routes>
       </Container>
     </React.Fragment>
